e719e4bdf771fd1cb10f50d9cb5febac3d7811bb
JacopoGobbi <jacopogobbi94@gmail.com>
2016-01-19 13:32:05 +0000

    Relation should work

--- app/models/follow_relation.rb
+++ app/models/follow_relation.rb
@@ -1,2 +1,7 @@
 class FollowRelation < ActiveRecord::Base
+  belongs_to :user_following, class_name: "User"
+  belongs_to :user_followed, class_name: "User"
+  validates :user_following_id, presence: true
+  validates :user_followed_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :follow_relations,
+    class_name: "FollowRelation",
+    foreign_key: "user_following_id",
+    dependent: :destroy
+  has_many :followers, through: :follow_relations, source: :user_following
+  has_many :following, through: :follow_relations, source: :user_followed
 end
