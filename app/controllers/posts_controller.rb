7f868dc946dd754f9a4a0b695b6c4a4278a09670
JacopoGobbi <jacopogobbi94@gmail.com>
2016-01-19 12:48:04 +0000

    fix for posts creator

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = Post.new(post_params.merge(user_id: session[:user_id]))
 
     respond_to do |format|
       if @post.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @posts = Post.all
+    @posts = Post.where(user_id: @user.id)
   end
 
   # GET /users/new
